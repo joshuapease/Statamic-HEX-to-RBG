040efdc81c3f36a97e0788a85a4ed261c07c77d5
Joshua Pease <joshua.pease@gmail.com>
2015-01-07 12:32:34 -0800

    Adds alpha and raw parameters

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ Quite helpful when used with the color fieldtype.
 It accepts both 3 and 6 character HEX values.
 
 ###Usage
+This modifier accepts two optional paremeters. The first is an alpha level and returns `rgba( )`. Set the second paremeter to `raw` if you wish to prevent `rgb()` or `rgba()` from being added to the string.
+
 ```
 {{ color_field|hexrgb }}
 ```
+`returns rgb(12,34,56);`
+
+```
+{{ color_field|hexrgb:.78 }}
+```
+`returns rgba(12,34,56,.78)`
+
+```
+{{ color_field|hexrgb:1:raw }}
+```
+`returns 12,34,56,1`
--- hexrgb/mod.hexrgb.php
+++ hexrgb/mod.hexrgb.php
@@ -3,19 +3,28 @@ class Modifier_hexrgb extends Modifier
 {
     public function index($value, $parameters=array())
     {
+      $hex = str_replace("#", "", $value);
+      $prefix = "rgb";
 
-       $hex = str_replace("#", "", $value);
+      if(strlen($hex) == 3) {
+        $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+        $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+        $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+      } else {
+        $r = hexdec(substr($hex,0,2));
+        $g = hexdec(substr($hex,2,2));
+        $b = hexdec(substr($hex,4,2));
+      }
+      $rgb = array($r, $g, $b);
+      $returnValue = implode(",", $rgb); // returns the rgb values separated by commas
 
-       if(strlen($hex) == 3) {
-          $r = hexdec(substr($hex,0,1).substr($hex,0,1));
-          $g = hexdec(substr($hex,1,1).substr($hex,1,1));
-          $b = hexdec(substr($hex,2,1).substr($hex,2,1));
-       } else {
-          $r = hexdec(substr($hex,0,2));
-          $g = hexdec(substr($hex,2,2));
-          $b = hexdec(substr($hex,4,2));
-       }
-       $rgb = array($r, $g, $b);
-       return implode(",", $rgb); // returns the rgb values separated by commas
+      if( isset($parameters[0]) ){
+        $returnValue .= "," . $parameters[0];
+        $prefix = "rgba";
+      }
+      if( !isset($parameters[1]) ) {
+        $returnValue = $prefix . "(" . $returnValue . ")";
+      }
+      return $returnValue;
     }
 }
